af76ef1f7ab01032188d52e1dbb2552cd62edbb9
janetatomas11 <janetatomas11@gmail.com>
2021-11-17 10:42:43 +0100

    Install script done

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,35 @@ WORKDIR thesis
 
 RUN apt update && apt install sudo
 
+RUN useradd nicouser
+
+RUN echo 'nicouser ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+USER nicouser
+
+RUN sudo apt update && DEBIAN_FRONTEND=noninteractive sudo apt install -y tzdata keyboard-configuration
+
+RUN	sudo apt update && sudo apt install -y vim wget curl git python3 python3-pip python3-venv lsb-release \
+	portaudio19-dev python3-pyaudio libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg python3-empy
+
+RUN sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list' && \
+	curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add - && \
+	sudo apt update && \
+	sudo apt install ros-noetic-desktop-full -y && \
+	bash /opt/ros/noetic/setup.bash && \
+	sudo apt install ros-noetic-moveit -y
+
+WORKDIR /home/nicouser/thesis
+
+RUN sudo chown -R nicouser:nicouser /home/nicouser
+RUN sudo chmod 777 /home/nicouser
+
+RUN wget https://www.coppeliarobotics.com/files/CoppeliaSim_Edu_V4_2_0_Ubuntu20_04.tar.xz
+
+RUN tar -xvf CoppeliaSim_Edu_V4_2_0_Ubuntu20_04.tar.xz
+
+RUN pip install virtualenv
+
+RUN git clone https://github.com/janetatomas11/NICO-software.git
+
 ADD install.sh .
--- install.sh
+++ install.sh
@@ -15,12 +15,23 @@ NICO_ROOT=${THESIS_ROOT}/${NICO_DIR}
 NICO_API_SRC=${NICO_DIR}/api/src
 NICO_INSTALL_SH=${NICO_DIR}/api/NICO-python3.bash
 
-
 ROS_SETUP_BASH=/opt/ros/noetic/setup.bash
 
 
-export DEBIAN_FRONTEND=noninteractive
-export COPPELIASIM_ROOT=${COPPELIASIM_ROOT} >> ${HOME}/.bashrc
+PY_REP_LINK=https://github.com/stepjam/PyRep.git
+PY_REP_DIR=PyRep
+PY_REP_ROOT=${THESIS_ROOT}/${PY_REP_DIR}
+
+
+VENV=${HOME}/.NICO-python3
+VENV_ACTIVATE=${VENV}/bin/activate
+
+
+export COPPELIASIM_ROOT=${COPPELIASIM_ROOT}
+echo export COPPELIASIM_ROOT=${COPPELIASIM_ROOT} >> ${HOME}/.bashrc
+echo export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$COPPELIASIM_ROOT >> ${HOME}/.bashrc
+echo export QT_QPA_PLATFORM_PLUGIN_PATH=$COPPELIASIM_ROOT >> ${HOME}/.bashrc
+
 
 function install_ros() {
 	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -28,14 +39,26 @@ function install_ros() {
 	sudo apt update
 	sudo apt install ros-noetic-desktop-full -y
 	source ${ROS_SETUP_BASH}
+	sudo apt install ros-noetic-moveit -y
 }
 
 
+function install_pyrep() {
+	if [ ! -d ${PY_REP_ROOT} ]
+	then
+		git clone ${PY_REP_LINK}
+	fi
+	cd ${PY_REP_ROOT}
+	pip install -r requirements.txt
+	pip install .
+	cd -
+}
+
 function install_deps() {
-	sudo apt update && sudo apt install -y vim wget curl git libportaudio2 python3 python3-pip \
-	portaudio19-dev python3-pyaudio libasound-dev lsb-release sudo \
-	portaudio19-dev libportaudio2 libportaudiocpp0
-	pip3 install virtualenv
+	sudo apt update && DEBIAN_FRONTEND=noninteractive  sudo apt install -y tzdata keyboard-configuration
+	sudo apt update && sudo apt install -y vim wget curl git python3 python3-pip python3-venv lsb-release \
+	portaudio19-dev python3-pyaudio libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg python3-empy
+	sudo pip install virtualenv
 	install_ros
 }
 
@@ -55,12 +78,26 @@ function download_sources() {
 
 
 function install() {
-	echo export COPPELIASIM_ROOT=${COPPELIASIM_ROOT} >> ${HOME}/.bashrc
 	source ~/.bashrc
 	bash ${NICO_INSTALL_SH}
+}
+
+
+function build_html_docs() {
+	cd ${NICO_ROOT}
+	source ${HOME}/.bashrc
+	source api/activate.bash
+    source api/pyrep_env.bash
+	cd api-doc/
+	make html
+	cd -
 }
 
 
 install_deps
 download_sources
 install
+source ${VENV_ACTIVATE}
+pip install eigenpy defusedxml pyserial pypot
+install_pyrep
+build_html_docs
