987b611cac7bd13a2885f2659fad079fde806d70
janetatomas11 <janetatomas11@gmail.com>
2022-02-28 20:18:02 +0100

    Nico env

--- env.py
+++ env.py
@@ -1,7 +1,6 @@
 
 from gym import Env
 from nicomotion.Motion import Motion
-from nicotouch import optoforcesensors
 from gym.spaces import Dict, Box
 import numpy as np
 
@@ -19,7 +18,7 @@ FRACTION_MAX_SPEED = 0.02
 
 
 class NicoEnv(Env):
-    def __init__(self, config_file, scene_file, goal, joints=None):
+    def __init__(self, config_file, scene_file, joints=None):
         super(NicoEnv, self).__init__()
         self._scene = scene_file
         self._config = Motion.vrepRemoteConfig()
@@ -27,49 +26,38 @@ class NicoEnv(Env):
         self._config['headless'] = True
         self._robot = Motion(motorConfig=config_file, vrep=True, vrepConfig=self._config)
         self._joints = joints if joints is not None else self._robot.getJointNames()
-        self._initial_state = self._get_state()
         self._robot.startSimulation()
-        self.observation_space = Dict({
-            joint: Box(low=self._robot.getAngleLowerLimit(joint),
-                       high=self._robot.getAngleUpperLimit(joint),
-                       shape=(1,)) for joint in self._joints
-        })
-        self.action_space = Dict({
-            joint: Box(low=self._robot.getAngleLowerLimit(joint),
-                       high=self._robot.getAngleUpperLimit(joint),
-                       shape=(1,)) for joint in self._joints
-        })
-        self._goal_state = self.observation_space.sample()
-
+        self._n = len(self._joints)
+        self._low = np.array([angle if angle <= 0 else angle - 360 for angle in [self._robot.getAngleLowerLimit(joint) for joint in self._joints]])
+        self._high = np.array([angle if angle >= 0 else 360 + angle for angle in [self._robot.getAngleUpperLimit(joint) for joint in self._joints]])
+        self.observation_space = Box(low=self._low, high=self._high)
+        self.action_space = Box(low=self._low, high=self._high)
+        self._initial_state = self._get_state()
 
     def _get_state(self):
-        state = {}
-        for joint in self._joints:
-            state[joint] = self._robot.getAngle(joint)
-        return state
+        return np.array([self._robot.getAngle(joint) for joint in self._joints])
 
     def _set_state(self, state):
-        for joint in state.keys():
-            self._robot.setAngle(joint, state[joint], 0.02)
+        for i, joint in enumerate(self._joints):
+            self._robot.setAngle(joint, state[i], 0.02)
 
     def _check_state(self, state):
         current_state = self._get_state()
         return current_state == state
 
-    def compute_reward(self):
-        current = self._get_state()
-        return -np.linalg.norm(np.array([current[joint] for joint in self._joints]) -
-                              np.array([self._goal_state[joint] for joint in self._joints]))
+    def compute_reward(self, state):
+        return 0
 
     def step(self, action):
-        observation = self._set_state(action)
+        self._set_state(action)
+        observation = self._get_state()
         reward = self.compute_reward()
         done = abs(reward) < 0.1
         return observation, reward, done, {}
 
     def reset(self):
         self._robot.toSafePosition()
-        return self._initial_state
+        return self._get_state()
 
     def render(self, mode="human"):
         pass
