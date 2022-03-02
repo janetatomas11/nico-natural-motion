9e779850833ec3ee71c39f46daa8fc2047b45568
janetatomas11 <janetatomas11@gmail.com>
2022-03-02 13:57:24 +0100

    a

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ import numpy as np
 from stable_baselines3.common.env_checker import check_env
 
 config_dir = "configs"
-json_config = F"{config_dir}/json/nico_humanoid_vrep.json"
-scene = F"{config_dir}/v-rep/NICO-seated.ttt"
+json_config = F"{config_dir}/nico_humanoid_vrep.json"
+scene = F"{config_dir}/NICO-seated.ttt"
 
 env = NicoEnv(
     config_file=json_config,
     scene_file=scene,
-    goal=1,
     joints=['l_shoulder_y', 'l_shoulder_z', 'l_arm_x', 'l_elbow_y', 'l_wrist_z', 'l_wrist_x']
 )
