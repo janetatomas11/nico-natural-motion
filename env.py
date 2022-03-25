ca5944494552583579f2a0d001cfde5ea6833872
janetatomas11 <janetatomas11@gmail.com>
2022-03-25 16:21:51 +0100

    Nico

--- env.py
+++ env.py
@@ -1,4 +1,3 @@
-
 from gym import Env
 from nicomotion.Motion import Motion
 from gym.spaces import Dict, Box
@@ -13,12 +12,13 @@ available_joints = [
     'r_wrist_z', 'r_wrist_x', 'r_indexfingers_x', 'r_thumb_x'
 ]
 
-right_arm = ['r_shoulder_y', 'r_shoulder_z', 'r_arm_x', 'r_elbow_y', 'r_wrist_z', 'r_wrist_x', 'r_indexfingers_x', 'r_thumb_x']
-left_arm = ['l_shoulder_y', 'l_shoulder_z', 'l_arm_x', 'l_elbow_y', 'l_wrist_z', 'l_wrist_x', 'l_indexfingers_x', 'l_thumb_x']
+right_arm = ['r_shoulder_y', 'r_shoulder_z', 'r_arm_x', 'r_elbow_y', 'r_wrist_z', 'r_wrist_x', 'r_indexfingers_x',
+             'r_thumb_x']
+left_arm = ['l_shoulder_y', 'l_shoulder_z', 'l_arm_x', 'l_elbow_y', 'l_wrist_z', 'l_wrist_x', 'l_indexfingers_x',
+            'l_thumb_x']
 POS_MIN, POS_MAX = [-3., -3., -3.], [3.0, 3.0, 3.0]
 FRACTION_MAX_SPEED = 0.05
 
-import timeit
 
 class NicoEnv(Env):
     def __init__(self, config_file, scene_file, joints=None):
@@ -29,26 +29,32 @@ class NicoEnv(Env):
         self._config['shared_vrep_io'] = self.io
         self._config['vrep_scene'] = self._scene
         self._config['use_pyrep'] = True
+        self._config['headless'] = True
+
         self._robot = Motion(motorConfig=config_file, vrep=True, vrepConfig=self._config)
         self._joints = joints if joints is not None else self._robot.getJointNames()
         self.handle = self.io.get_object('r_thumb_x')
-        self.io.add_sphere(name='target', size=[1, 1, 1])
         self.target = self.io.get_object('target')
         self._n = len(self._joints)
+        self.initial_position = self._get_state()[0:self._n]
         self.threshold = 0.2
-        self._low = np.array([angle if angle <= 0 else angle - 360 for angle in [self._robot.getAngleLowerLimit(joint) for joint in self._joints]])
-        self._high = np.array([angle if angle >= 0 else 360 + angle for angle in [self._robot.getAngleUpperLimit(joint)
-                                                                                  for joint in self._joints]])
-        self.observation_space = Box(low=np.array([POS_MIN, POS_MIN]), high=np.array([POS_MAX, POS_MAX]))
+
+        self._low = np.array([angle if angle <= 0 else angle - 360 for angle in
+                              [self._robot.getAngleLowerLimit(joint) for joint in self._joints]])
+        self._high = np.array([angle if angle >= 0 else 360 + angle for angle in
+                               [self._robot.getAngleUpperLimit(joint) for joint in self._joints]])
+
+        self.observation_space = Box(low=np.array(2 * POS_MIN), high=np.array(2 * POS_MAX))
         self.action_space = Box(low=self._low, high=self._high)
 
     def _execute_action(self, action):
         for angle, joint in zip(action, self._joints):
             self._robot.setAngle(jointName=joint, angle=angle, fractionMaxSpeed=FRACTION_MAX_SPEED)
-        self.io.pyrep.step()
+        for i in range(self._n):
+            self.io.pyrep.step()
 
     def _get_state(self):
-        return np.array([self.handle.get_position(), self.target.get_position()])
+        return np.concatenate([self.handle.get_position(), self.target.get_position()])
 
     def step(self, action):
         self._execute_action(action)
@@ -56,12 +62,14 @@ class NicoEnv(Env):
         distance = np.linalg.norm(self.handle.get_position(self.target))
         reward = -distance
         done = distance <= self.threshold
+        print(reward)
         return observation, reward, done, {}
 
     def reset(self):
         observation = self.observation_space.sample()
-        self.handle.set_position(observation[0])
-        self.target.set_position(observation[1])
+        self.handle.set_position(observation[0:self._n])
+        self.io.pyrep.step()
+        # self.target.set_position(observation[1])
         return self._get_state()
 
     def render(self, mode='human'):
@@ -70,5 +78,3 @@ class NicoEnv(Env):
     def close(self):
         self._robot.stopSimulation()
         self._robot.cleanup()
-
-
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from env import NicoEnv, left_arm
 from os.path import dirname, abspath
 import numpy as np
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3 import TD3
 
 config_dir = "configs"
 json_config = F"{config_dir}/nico_humanoid_vrep.json"
@@ -14,3 +15,6 @@ env = NicoEnv(
     scene_file=scene,
     joints=['l_shoulder_y', 'l_shoulder_z', 'l_arm_x', 'l_elbow_y', 'l_wrist_z', 'l_wrist_x']
 )
+
+model = TD3("MlpPolicy", env=env, verbose=1)
+model.learn(total_timesteps=10000, log_interval=10)
