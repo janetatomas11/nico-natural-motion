935e77c71fc08f751a0840f6507f854830af0dc1
janetatomas11 <janetatomas11@gmail.com>
2022-04-03 19:58:13 +0200

    Forum fixes

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python
 
-from env import NicoEnv
 from stable_baselines3 import TD3
 from simple_reacher import PandaReacher
-config_dir = "configs"
-json_config = F"{config_dir}/nico_humanoid_vrep.json"
-scene = F"{config_dir}/NICO-standing.ttt"
-
-# env = NicoEnv(
-#     config_file=json_config,
-#     scene_file=scene,
-#     joints=['l_shoulder_z', 'l_arm_x', 'l_elbow_y', 'l_wrist_z'],
-# )
 
 env = PandaReacher(scene_file='../scene_reinforcement_learning_env.ttt')
 model = TD3("MlpPolicy", env=env, verbose=1, learning_rate=0.01)
