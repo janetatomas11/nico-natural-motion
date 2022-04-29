f6b68c07f9070092defd4752df7ce412950d80cb
janetatomas11 <janetatomas11@gmail.com>
2022-04-29 10:00:55 +0200

    jaco

--- main.py
+++ main.py
@@ -1,19 +1,73 @@
-#!/usr/bin/env python
-
-from env import NicoEnv, left_arm
-from os.path import dirname, abspath
+import time
+from os.path import dirname, join, abspath
+from pyrep import PyRep
+from pyrep.robots.arms.jaco import Jaco
+from pyrep.objects.shape import Shape
 import numpy as np
-from stable_baselines3 import TD3
 
-config_dir = "configs"
-json_config = F"{config_dir}/nico_humanoid_vrep.json"
-scene = F"{config_dir}/NICO-standing.ttt"
+SCENE_FILE = join(dirname(abspath(__file__)),
+                  'scene_jaco.ttt')
+POS_MIN, POS_MAX = [0.8, -0.2, 1.0], [1.0, 0.2, 1.4]
+EPISODES = 5
+EPISODE_LENGTH = 200
 
-env = NicoEnv(
-    config_file=json_config,
-    scene_file=scene,
-    joints=['l_shoulder_z', 'l_arm_x', 'l_elbow_y', 'l_wrist_z'],
-)
 
-model = TD3("MlpPolicy", env=env, verbose=1, learning_rate=0.01)
-model.learn(total_timesteps=10000)
+class ReacherEnv(object):
+
+    def __init__(self):
+        self.pr = PyRep()
+        self.pr.launch(SCENE_FILE, headless=False)
+        self.pr.start()
+        self.agent = Jaco()
+        self.agent.set_control_loop_enabled(False)
+        self.agent.set_motor_locked_at_zero_velocity(True)
+        self.agent_ee_tip = self.agent.get_tip()
+        self.initial_joint_positions = self.agent.get_joint_positions()
+
+    def _get_state(self):
+        # Return state containing arm joint angles/velocities & target position
+        return np.concatenate([self.agent.get_joint_positions(),
+                               self.agent.get_joint_velocities(),
+                               self.target.get_position()])
+
+    def reset(self):
+        # Get a random position within a cuboid and set the target position
+        pos = list(np.random.uniform(POS_MIN, POS_MAX))
+        self.target.set_position(pos)
+        self.agent.set_joint_positions(self.initial_joint_positions)
+        return self._get_state()
+
+    def step(self, action):
+        self.agent.set_joint_target_velocities(action)  # Execute action on arm
+        self.pr.step()  # Step the physics simulation
+        ax, ay, az = self.agent_ee_tip.get_position()
+        tx, ty, tz = self.target.get_position()
+        # Reward is negative distance to target
+        reward = -np.sqrt((ax - tx) ** 2 + (ay - ty) ** 2 + (az - tz) ** 2)
+        return reward, self._get_state()
+
+    def shutdown(self):
+        self.pr.stop()
+        self.pr.shutdown()
+
+
+class Agent(object):
+
+    def act(self, state):
+        del state
+        return list(np.random.uniform(-1.0, 1.0, size=(7,)))
+
+    def learn(self, replay_buffer):
+        del replay_buffer
+        pass
+
+
+env = ReacherEnv()
+agent = Agent()
+replay_buffer = []
+
+for e in range(EPISODES):
+    for i in range(EPISODE_LENGTH):
+        time.sleep(1)
+        print(1)
+
